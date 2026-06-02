cda040b811162d978752408c0f857be3ddd85d28
Maria Khomynets <masha.khomynets@gmail.com>
2026-06-02 10:43:26 +0300

    add rask1 and task2

---
+++ src/task-1.ts
@@ -0,0 +1,12 @@
+// Задача 1. Типізація скалярних значень (файл task-1.ts)
+// const name = "Alice";
+// const age = 30;
+// const isOnline = true;
+
+const name: string = "Alice";
+const age: number = 30;
+const isOnline: boolean = true;
+
+console.log(`Name: ${name}`);
+console.log(`Age: ${age}`);
+console.log(`Online: ${isOnline}`);
---
+++ src/task-2.ts
@@ -0,0 +1,13 @@
+// Задача 2. Інтерфейси (файл task-2.ts)
+interface Product {
+  readonly id: number;
+  title: string;
+  description?: string;
+}
+const product: Product = {
+  id: 1,
+  title: "Tablet",
+  description: "Compact and fast",
+};
+
+console.log(`Product: ${JSON.stringify(product)}`);
