e81cdbff75218bc4e133d5c011c4b51febe8cbe7
Maria Khomynets <masha.khomynets@gmail.com>
2026-06-04 11:31:30 +0300

    task 3 task 4

---
+++ src/task-5.ts
@@ -0,0 +1,1 @@
+// Задача 5. Union Type (файл task-5.ts)
---
+++ src/task-8.ts
@@ -0,0 +1,11 @@
+// Задача 4. Функції (файл task-4.ts)
+function printUserInfo(name: string, age: number, email?: string): void {
+  console.log("Name:", name);
+  console.log("Age:", age);
+  if (email) {
+    console.log("Email:", email);
+  }
+}
+
+printUserInfo("Alice", 30);
+printUserInfo("Bob", 25, "bob@mail.com");
