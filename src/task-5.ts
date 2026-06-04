b218b9f329a23dff699a477adfdf6d10d720c9bb
Maria Khomynets <masha.khomynets@gmail.com>
2026-06-04 11:42:49 +0300

    task5

--- src/task-5.ts
+++ src/task-5.ts
@@ -1,1 +1,12 @@
 // Задача 5. Union Type (файл task-5.ts)
+function logStatus(status: "loading" | "success" | "error"): void {
+  if (status === "loading") {
+    console.log("Loading...");
+  } else if (status === "success") {
+    console.log("Success!");
+  } else if (status === "error") {
+    console.log("Something went wrong");
+  }
+}
+
+logStatus("loading");
